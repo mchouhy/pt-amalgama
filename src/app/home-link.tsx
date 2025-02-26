bb65c7fbf73c9238fec6e7cfd69a3f139dde6abe
mchouhy <marianochouhy@gmail.com>
2025-02-26 18:46:07 +0100

    add logo

--- src/app/home-link.tsx
+++ src/app/home-link.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function HomeLink() {
   return (
     <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-        Amalgama
+      <span className="font-medium block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        Inicio
       </span>
     </Link>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { SvgIconAmalgamaLogo } from "@/components/ui/icons";
+
 export default async function HomePage() {
   return (
     <main className="flex flex-col justify-end items-center mx-auto h-[50vh]">
-      Home
+      <SvgIconAmalgamaLogo className="w-80 md:w-96" />
     </main>
   );
 }
