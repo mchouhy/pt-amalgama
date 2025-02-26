73fc0358fdd0e19fe79bcbd08e8e63127fe936bb
mchouhy <marianochouhy@gmail.com>
2025-02-26 18:51:33 +0100

    delete unused console.log

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -4,7 +4,6 @@ import { cookies } from "next/headers";
 export async function GET() {
   const cookieStore = cookies();
   const email = (await cookieStore).get("user-email")?.value;
-  console.log("COOKIES", { cookieStore, email });
   if (!email) {
     return NextResponse.json(
       { error: "No hay usuario logueado" },
